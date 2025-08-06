6d5010ead2654712dd7715bb204f6740bf678b00
w0lff01ll3 <d1m1tr1.d3l3tang@gmail.com>
2025-08-07 00:17:19 +0200

    definitely i love daisyui, update ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,100 @@
+import { ScanLine, Search } from "lucide-react";
+import Sidebar from "./components/Sidebar";
+
 export default function App()
 {
   return(
-    <div>
+    <div className="flex h-screen">
+      <Sidebar/>
+      <main className="flex flex-col w-full">
+        <header className="flex items-center justify-start max-h-12 min-h-12 bg-base-200 border-b border-b-base-300 p-4">
+          <h1 className="text-md font-bold">Fiche article</h1>
+        </header>
+        
+        <div className="flex p-4 gap-4 h-full">
+          <form className="w-3/6 flex flex-col gap-4">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border px-4 pb-4">
+              <legend className="fieldset-legend">Code produit</legend>
+              
+              <label className="input focus:outline-none">
+                <ScanLine className="h-[1em]"/>
+                <input className="focus:outline-none" type="search" required placeholder="Scannez pour rechercher" />
+              </label>
+            </fieldset>
 
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border px-4 pb-4">
+              <legend className="fieldset-legend">Informations</legend>
+              
+              <label className="label flex flex-col items-start">
+                <span>Nom</span>
+                <input className="input" type="name" required placeholder="Jus d'orange 1L" />
+              </label>
+              <label className="label flex flex-col items-start">
+                <span>Nom (étiquette)</span>
+                <input className="input" type="label-name" required placeholder="Jus d'orange 1L" />
+              </label>
+              <label className="label flex flex-col items-start">
+                <span>Marque</span> 
+                <select className="select" name="marque">
+                  <option disabled selected>Définir la marque</option>
+                </select>
+              </label>
+            </fieldset>
+
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border px-4 pb-4">
+              <legend className="fieldset-legend">Catégorie</legend>
+              
+              <label className="label flex flex-col items-start">
+                <span>Rayon</span> 
+                <select className="select" name="rayon">
+                  <option disabled selected>Définir le rayon</option>
+                </select>
+              </label>
+              <label className="label flex flex-col items-start">
+                <span>Gamme</span> 
+                <select className="select" name="gamme">
+                  <option disabled selected>Définir la gamme</option>
+                </select>
+              </label>
+              <label className="label flex flex-col items-start">
+                <span>Type</span> 
+                <select className="select" name="type">
+                  <option disabled selected>Définir le type</option>
+                </select>
+              </label>
+            </fieldset>
+
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border px-4 pb-4">
+              <legend className="fieldset-legend">Fournisseur</legend>
+              
+              <label className="label flex flex-col items-start">
+                <span>Fournisseur</span> 
+                <select className="select" name="fournisseur">
+                  <option disabled selected>Définir le fournisseur</option>
+                </select>
+              </label>
+
+              <label className="label flex flex-col items-start">
+                <span>Référence</span>
+                <input className="input" type="name" required placeholder="123456dsd58" />
+              </label>
+            </fieldset>
+          </form>
+
+          <div className="w-full h-full">
+            <div className="tabs tabs-lift h-full">
+              <input type="radio" name="my_tabs_3" className="tab" aria-label="Prix" defaultChecked/>
+              <div className="tab-content bg-base-100 border-base-300 p-6">Prix</div>
+
+              <input type="radio" name="my_tabs_3" className="tab" aria-label="Stocks" />
+              <div className="tab-content bg-base-100 border-base-300 p-6">Stocks</div>
+
+              <input type="radio" name="my_tabs_3" className="tab" aria-label="Étiquette" />
+              <div className="tab-content bg-base-100 border-base-300 p-6">Étiquette</div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,134 @@
+import { Package, Boxes, Users, Truck, ReceiptEuro, Megaphone, Share2, Tag } from "lucide-react";
+
+const GROUPED_MENUS = [
+  {
+    namespace: "Magasin",
+    menus: [
+      {
+        title: "Articles",
+        icon: Package,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      },
+      {
+        title: "Stocks",
+        icon: Boxes,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      },
+      {
+        title: "Clients",
+        icon: Users,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      },
+      {
+        title: "Commandes",
+        icon: Truck,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      }
+    ]
+  },
+  {
+    namespace: "Comptabilité",
+    menus: [
+      {
+        title: "Factures",
+        icon: ReceiptEuro,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      }
+    ]
+  },
+  {
+    namespace: "Réseau",
+    menus: [
+      {
+        title: "Communication",
+        icon: Megaphone,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      },
+      {
+        title: "Centre de partage",
+        icon: Share2,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      }
+    ]
+  },
+  {
+    namespace: "Outils",
+    menus: [
+      {
+        title: "Étiquettes",
+        icon: Tag,
+        items: [
+          { title: "Gestion", href: "/" }
+        ]
+      }
+    ]
+  },
+];
+
+export default function Sidebar()
+{
+  return(
+    <aside className="flex flex-col bg-[#fff5e3]/15 border-r border-r-base-300 w-64">
+      <header className="bg-base-200">
+        <a className="flex items-center justify-center h-12 p-2 text-sm" href="/">
+          On verra plus tard pour ça
+        </a>
+      </header>
+
+      {GROUPED_MENUS.map((group, groupId) => (
+        <nav className="menu text-base-content w-full pb-0">
+          <span className="menu-title text-[#f18a23]/90 text-[.75em] font-bold uppercase">{group.namespace}</span>
+          
+          {group.menus.map((menu, menuId) => (
+            <ul>
+              <li>
+                <details open={groupId === 0 && menuId === 0}>
+                  <summary>
+                    <menu.icon className="size-[1.25em]"/>
+                    {menu.title}
+                  </summary>
+
+                  <ul>
+                    {menu.items.map((link) => (
+                      <li key={link.title}>
+                        <a href={link.href}>
+                          {link.title}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          ))}
+        </nav>
+      ))}
+
+      <footer className="flex items-center mt-auto p-4 gap-2 bg-base-300 border-t border-t-base-300">
+        <div className="avatar avatar-online">
+          <div className="w-8 rounded-full">
+            <img src="https://img.daisyui.com/images/profile/demo/gordon@192.webp" />
+          </div>
+        </div>
+        <div className="flex flex-col w-full">
+          <span className="text-xs">THOREAU Amélie</span>
+          <span className="text-xs">commandes.lgpbloches@gmail.com</span>
+        </div>
+      </footer>
+    </aside>
+  );
+};
