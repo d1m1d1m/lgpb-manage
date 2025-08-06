cb681b3ce29ef4a95d6f7d369e16a6400e6c057a
w0lff01ll3 <d1m1tr1.d3l3tang@gmail.com>
2025-08-06 21:59:04 +0200

    finally i prefer daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 export default function App()
 {
   return(
-    <main className="flex">
-      <div className="w-1/2">
-        <h1>salut</h1>
-      </div>
+    <div>
 
-      <div className="w-1/2">
-        <h1>world</h1>
-      </div>
-    </main>
+    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router";
 import './index.css';
 import App from './App.tsx';
-import AppLayout from './components/AppLayout.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <AppLayout>
-      <App/>
-    </AppLayout>
+    <App/>
   </BrowserRouter>
 );
